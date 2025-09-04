3430eee23076aa93274c8279aba819b0cb93b649
Neemias Vasconcelos <167483565+neemo-o@users.noreply.github.com>
2025-09-04 20:45:16 -0300

    Add botoes do menu, novos assets --> menu finalizado

--- src/main/Game.java
+++ src/main/Game.java
@@ -15,6 +15,9 @@ public class Game implements Mediator {
 
 	@Override
     public void startGame() {
+        // TODO: RAMON Implementar o cenário do jogo aqui e iniciar o loop principal do jogo
+        // Lembrar de usar o Mediator que eu criei para comunicar entre as classes
+        // Se possivel tente usar o padrão Composite em algum funcionalidade do jogo
         System.out.println("Teste1");
     }
 
--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -2,8 +2,8 @@ package ui;
 
 import main.Mediator;
 import javax.swing.*;
+import java.awt.event.*;
 import java.awt.*;
-
 
 public class MainMenu {
     private Mediator mediator;
@@ -29,17 +29,67 @@ public class MainMenu {
                 ImageIcon background = new ImageIcon("src/assets/background.png");
                 g.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this);
 
-                ImageIcon logo = new ImageIcon("src/assets/logo2.png"); // Replace with your logo file path
+                ImageIcon logo = new ImageIcon("src/assets/logo2.png"); 
                 int logoWidth = logo.getIconWidth() / 2;
                 int logoHeight = logo.getIconHeight() / 2;
-                int x = (getWidth() - logoWidth) / 2; 
-                int y = (getHeight() - logoHeight) / 5; 
+                int x = (getWidth() - logoWidth) / 2;
+                int y = (getHeight() - logoHeight) / 14;
                 g.drawImage(logo.getImage(), x, y, logoWidth, logoHeight, this);
             }
         };
+        panel.setLayout(new GridBagLayout()); 
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(2, 10, 2, 10);
 
 
-        panel.setLayout(null);
+        ImageIcon startNormal = new ImageIcon("src/assets/PlayBtn.png");
+        ImageIcon startHover = new ImageIcon("src/assets/PlayClick.png"); 
+        Image startImageHover = startHover.getImage().getScaledInstance(150, 60, Image.SCALE_SMOOTH); 
+        ImageIcon scaledStartNormalHover = new ImageIcon(startImageHover);
+        Image startImage = startNormal.getImage().getScaledInstance(150, 60, Image.SCALE_SMOOTH); 
+        ImageIcon scaledStartNormal = new ImageIcon(startImage);
+        JButton startButton = new JButton(scaledStartNormal);
+        startButton.setRolloverIcon(scaledStartNormalHover);
+        startButton.setBorderPainted(false);
+        startButton.setContentAreaFilled(false);
+        startButton.setBounds(500, 300, 100, 40);
+        startButton.setFocusPainted(false);
+        gbc.gridx = 0;
+        gbc.gridy = 1; 
+        gbc.anchor = GridBagConstraints.CENTER; 
+        panel.add(startButton, gbc);
+
+
+        ImageIcon exitNormal = new ImageIcon("src/assets/ExitBtn.png");
+        ImageIcon exitHover = new ImageIcon("src/assets/ExitClick.png"); 
+        Image exitImageHover = exitHover.getImage().getScaledInstance(150, 60, Image.SCALE_SMOOTH); 
+        ImageIcon scaledExitNormalHover = new ImageIcon(exitImageHover);
+        Image exitImage = exitNormal.getImage().getScaledInstance(150, 60, Image.SCALE_SMOOTH); 
+        ImageIcon scaledExitNormal = new ImageIcon(exitImage);
+        JButton exitButton = new JButton(scaledExitNormal);
+        exitButton.setRolloverIcon(scaledExitNormalHover);
+        exitButton.setBorderPainted(false);
+        exitButton.setContentAreaFilled(false);
+        exitButton.setFocusPainted(false);
+        gbc.gridy = 2; 
+        panel.add(exitButton, gbc);
+
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mediator.startGame();
+                frame.dispose();
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0); 
+            }
+        });
+
         frame.add(panel);
         frame.setVisible(true);
     }
