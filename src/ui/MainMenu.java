8b7a0848a819601cacf83f33739cad14671d77a9
Neemias Vasconcelos <167483565+neemo-o@users.noreply.github.com>
2025-09-04 19:28:42 -0300

    Add logos, logo to menu

--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -1,7 +1,9 @@
 package ui;
+
 import main.Mediator;
 import javax.swing.*;
 import java.awt.*;
+
 
 public class MainMenu {
     private Mediator mediator;
@@ -10,10 +12,12 @@ public class MainMenu {
         this.mediator = mediator;
     }
 
-     public void show() {
+    public void show() {
         System.out.println("Iniciando menu");
 
+        ImageIcon icon = new ImageIcon("src/assets/logo.png");
         JFrame frame = new JFrame("InsecTD");
+        frame.setIconImage(icon.getImage());
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
@@ -24,13 +28,20 @@ public class MainMenu {
                 super.paintComponent(g);
                 ImageIcon background = new ImageIcon("src/assets/background.png");
                 g.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this);
+
+                ImageIcon logo = new ImageIcon("src/assets/logo2.png"); // Replace with your logo file path
+                int logoWidth = logo.getIconWidth() / 2;
+                int logoHeight = logo.getIconHeight() / 2;
+                int x = (getWidth() - logoWidth) / 2; 
+                int y = (getHeight() - logoHeight) / 5; 
+                g.drawImage(logo.getImage(), x, y, logoWidth, logoHeight, this);
             }
         };
+
 
         panel.setLayout(null);
         frame.add(panel);
         frame.setVisible(true);
-     }
+    }
 
-        
 }
