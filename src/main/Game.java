92c57682dc676b0f945b0290696188f842ccb4e8
Neemias Vasconcelos <167483565+neemo-o@users.noreply.github.com>
2025-09-06 13:44:53 -0300

    Add parte de Ramon

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,21 +1,26 @@
 package main;
+
 import ui.MainMenu;
 
 public class Game implements Mediator {
 
-	public static void main(String[] args) {
-		Game jogo = new Game();
-		jogo.showMainMenu();
-	}
+    public static void main(String[] args) {
+        Game jogo = new Game();
+        jogo.showMainMenu();
+    }
 
-	public void showMainMenu() {
+    public void showMainMenu() {
         MainMenu menu = new MainMenu(this);
         menu.show();
     }
 
-	@Override
+    @Override
     public void notify(Object sender, String event) {
         if (sender instanceof MainMenu && event.equals("startGame")) {
+            // TODO: RAMON Implementar o cenário do jogo aqui e iniciar o loop principal do
+            // jogo
+            // Lembrar de usar o Mediator que eu criei para comunicar entre as classes
+            // Se possivel tente usar o padrão Composite em algum funcionalidade do jogo
             System.out.println("Iniciando o jogo...");
         }
     }
