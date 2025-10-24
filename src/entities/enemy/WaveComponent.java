93669756e0bc58d8b6b43bc043feb8b3491849a1
Gustavo Ramon <so18989898@gmail.com>
2025-10-23 22:10:44 -0300

    informaçao da wave

--- src/entities/enemy/WaveComponent.java
+++ src/entities/enemy/WaveComponent.java
@@ -3,7 +3,7 @@ package entities.enemy;
 import java.util.List;
 
 import entities.wave.SpawnEvent;
-
+// Interface para componentes de ondas de inimigos, nada demais.
 public interface WaveComponent {
     void scheduleSpawns(List<SpawnEvent> outEvents, long waveStartTimeMs);
 }
