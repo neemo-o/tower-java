abbfcbab5e32e250a141c4e32983548cb8018f94
Gustavo Ramon <so18989898@gmail.com>
2025-10-23 22:40:47 -0300

    comentando o mediador

--- src/main/Mediator.java
+++ src/main/Mediator.java
@@ -1,5 +1,5 @@
 package main;
-
+// interface do carinha que comunica os componentes entre eles. as classes sao cegas sem o mediador presente
 public interface Mediator {
     void notify(Object sender, String event);
 }
